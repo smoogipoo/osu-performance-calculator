016129c5fa1c7d586bbc4dcb3fbe032f0ad76826
smoogipoo <smoogipoo@smgi.me>
2021-07-17 00:24:26 +0900

    Init

---
+++ osu.Server.PerformanceCalculator/Program.cs
@@ -0,0 +1,12 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace osu.Server.PerformanceCalculator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
